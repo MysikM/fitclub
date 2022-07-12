0abef26e60352914d84cbff99023356219202061
MysikM <46257801+MysikM@users.noreply.github.com>
2022-07-12 10:21:42 +0300

    create left hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,48 @@
 import './Hero.css';
 import Header from "../Header/Header";
+import heroImage from '../../assets/hero_image.png';
+import heroImageBack from '../../assets/hero_image_back.png';
+import heart from '../../assets/heart.png';
+import calories from '../../assets/calories.png';
 
 const Hero = () => {
     return (
         <div className="hero">
             <div className="left-h">
                 <Header />
+
+                <div className="the-best-ad">
+                    <div/>
+                    <span>the best fitness club in the town</span>
+                </div>
+
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                    <div>
+                        <span>
+                            In here we will help you to shape and build your ideal body and live up your life to fullest
+                        </span>
+                    </div>
+                </div>
+
+                <div className="figure">
+                    <div><span>+140</span><span>EXPERT COACHES</span></div>
+                    <div><span>+978</span><span>MEMBERS JOINED</span></div>
+                    <div><span>+50</span><span>FITNESS PROGRAMS</span></div>
+                </div>
+
+                <div className="hero-buttons">
+                    <button className="btn">Get Started</button>
+                    <button className="btn">Learn More</button>
+                </div>
             </div>
             <div className="right-h">
-                right side
             </div>
         </div>
     );
