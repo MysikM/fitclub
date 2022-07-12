a27cb5044f1be8450f434484aa45244755004684
MysikM <46257801+MysikM@users.noreply.github.com>
2022-07-12 15:08:58 +0300

    create footer section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import './footer.css';
+import gitHub from '../../assets/github.png';
+import instagram from '../../assets/instagram.png';
+import linkedIn from '../../assets/linkedin.png';
+import logo from '../../assets/logo.png';
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <hr/>
+            <div className="footer">
+                <div className="social-links">
+                    <img src={gitHub} alt="gitHub icon"/>
+                    <img src={instagram} alt="instagram icon"/>
+                    <img src={linkedIn} alt="linkedIn icon"/>
+                </div>
+                <div className="logo-f">
+                    <img src={logo} alt="logo icon"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
